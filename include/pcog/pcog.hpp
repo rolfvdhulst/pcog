5f10cd82b57b946f5e47535be159fb8f9653dcc5
rolf <rolfvdhulst@gmail.com>
2022-11-18 19:19:12 +0100

    Fix some doxygen things

--- include/pcog/pcog.hpp
+++ include/pcog/pcog.hpp
@@ -5,7 +5,26 @@
 #ifndef PCOG_SRC_PCOG_HPP
 #define PCOG_SRC_PCOG_HPP
 
+#include <pcog/export.h>
+
+
+/// \brief pcog is the main nameSpace
+/// we work in
+
 namespace pcog {
+
+/// \brief Class which holds an instance of PCOG
+/// Further testing necessary to determine if it works properly or not
+/// another line of comments
+class PCOG{
+ public:
+
+};
+
+/// \brief Adds two numbers a and b together
+/// \param a first number
+/// \param b Second number
+/// \return the sum, a+b
 int add(int a, int b);
 } // namespace pcog
 #endif // PCOG_SRC_PCOG_HPP
